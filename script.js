be5b5915ecb56c8e003aae18b89e9ee46aa84b8d
CandraMotor <dindinsamsudin106@gmail.com>
2026-02-20 14:23:46 +0700

    Update car data and adjust display functionality

--- script.js
+++ script.js
@@ -1,30 +1,59 @@
 const cars = [
     {
-        name: "Toyota Avanza 2022",
+        name: "Toyota Yaris 2011 Type E A/T",
         brand: "Toyota",
-        price: 220000000,
-        image: "https://source.unsplash.com/400x300/?toyota,avanza"
+        price: 11900000,
+        image: "https://i.ibb.co.com/cSGjXFpG/Whats-App-Image-2026-02-20-at-13-25-34.jpg"
     },
     {
-        name: "Honda Brio 2021",
+        name: "Honda Brio 2017 A/T",
         brand: "Honda",
-        price: 180000000,
-        image: "https://source.unsplash.com/400x300/?honda,brio"
+        price: 124000000,
+        image: "https://i.ibb.co.com/8gqqcXX6/Whats-App-Image-2026-02-20-at-13-25-28.jpg"
     },
     {
-        name: "Mitsubishi Pajero 2020",
+        name: "Mitsubishi Pajero 2015 DAKAR",
         brand: "Mitsubishi",
-        price: 450000000,
-        image: "https://source.unsplash.com/400x300/?mitsubishi,pajero"
+        price: 285000000,
+        image: "https://i.ibb.co.com/Gr3tH0k/Whats-App-Image-2026-02-20-at-13-25-40-1.jpg"
     },
 	{
-        name: "Mitsubishi Xpander 2025, mulus antik no minus",
+        name: "Mitsubishi Xpander 2018 type exide M/T",
         brand: "Mitsubishi",
         price: 240000000,
-        image: "https://i.ibb.co.com/9X0HV11/prev-desktop-harga-resmi-mitsubishi-expander-cover-2017-mitsubishi-expander-1.jpg"
-    }
+        image: "https://i.ibb.co.com/XrWCJ3s3/Whats-App-Image-2026-02-20-at-13-25-39-1.jpg"
+    },
+	{	
+		name: "Toyota Rush 2011 type G",
+		brand: "Toyota",
+		price: 12500000,
+		image: "https://i.ibb.co.com/ymsD9Fvq/Whats-App-Image-2026-02-20-at-13-25-39.jpg"
+	},
+	{
+		name: "GrandMax 2014",
+		brand: "Daihatsu",
+		price: 120000000,
+		image: "https://i.ibb.co.com/bRYjt9f9/Whats-App-Image-2026-02-20-at-13-25-31.jpg"
+	},
+	{
+		name: "Honda Jazz RS 2017 A/T",
+		brand: "Honda",
+		price: 198000000,
+		image: "https://i.ibb.co.com/Dg1LqWWv/Whats-App-Image-2026-02-20-at-13-25-37.jpg"
+	},
+	{
+		name: "Brio E satya 2018 A/T",
+		brand: "Honda",
+		price: 129000000,
+		image: "https://i.ibb.co.com/N6ydfdGf/Whats-App-Image-2026-02-20-at-13-25-35.jpg"
+	},
+	{
+		name: "Brio E satya 2021 M/T",
+		brand: "Honda",
+		price: 134000000,
+		image: "https://i.ibb.co.com/hxn07kYH/Whats-App-Image-2026-02-20-at-13-25-34-1.jpg"
+	}
 ];
-
 const carList = document.getElementById("carList");
 const searchInput = document.getElementById("search");
 const filterBrand = document.getElementById("filterBrand");
